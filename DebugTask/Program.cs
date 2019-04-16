7fc3e185381c92c6190f08b2c035f241c265d5be
Yauheni Papiunenka <yauheni.papiunenka@thomsonreuters.com>
2019-04-16 17:14:28 +0300

    Add project files.

---
+++ DebugTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
